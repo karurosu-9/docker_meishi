a2e9d54c5c905a2f588769afbdb47000f26e0361
carlos <cl0xtc43x.apxma@gmail.com>
2023-04-03 23:06:46 +0900

    show_branchのルーティングの変更

--- docker_meishi2/routes/web.php
+++ docker_meishi2/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MitumoriController;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -29,27 +30,13 @@ Route::post('mitumori/mitumori_toroku', [MitumoriController::class, 'mitumori_to
 Route::get('mitumori/mitumori_list', [MitumoriController::class, 'mitumori_list'])->name('mitumori.list');
 Route::post('mitumori/mitumori_list', [MitumoriController::class, 'mitumori_list']);
 Route::get('mitumori/mitumori_show', [MitumoriController::class, 'mitumori_show']);
-Route::post('mitumori/show_branch', function(){
+Route::post('mitumori/show_branch', [MitumoriController::class, 'show_branch']);
 
-if(isset($_POST['edit'])){
-    $disp_id = $_POST['id'];
-    header('Location:/mitumori/edit_kingaku?id='.$disp_id);
-    exit;
-}
-
-if(isset($_POST['delete'])){
-    $disp_id=$_POST['id'];
-    header('Location:/mitumori/mitumori_delete?id='.$disp_id);
-    exit;
-}
-
-});
-
-Route::get('mitumori/edit_kingaku', [MitumoriController::class, 'edit_kingaku']);
+Route::get('mitumori/edit_kingaku/{id}', [MitumoriController::class, 'edit_kingaku'])->name('edit.kingaku');
 Route::post('mitumori/edit_biko', [MitumoriController::class, 'edit_biko']);
 Route::post('mitumori/edit_hosoku', [MitumoriController::class, 'edit_hosoku']);
 Route::post('mitumori/edit_check', [MitumoriController::class, 'edit_check']);
 Route::post('mitumori/edit_check_confirm', [MitumoriController::class, 'edit_check_confirm']);
 Route::post('mitumori/update', [MitumoriController::class, 'update']);
 Route::post('mitumori/print', [MitumoriController::class, 'print']);
-Route::get('mitumori/mitumori_delete', [MitumoriController::class, 'mitumori_delete']);
+Route::get('mitumori/mitumori_delete', [MitumoriController::class, 'mitumori_delete'])->name('mitumori.delete');
